c29712e5daff52ee01c23c6b98483ff0b5fb1e32
akash_mathew <160788950+akashmathew18@users.noreply.github.com>
2025-03-19 09:20:48 +0530

    Update settings.py

--- JVBeatBazaar/settings.py
+++ JVBeatBazaar/settings.py
@@ -70,10 +70,16 @@ WSGI_APPLICATION = 'JVBeatBazaar.wsgi.application'
 
 
 # Database configuration
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # Change this based on your database type
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),  # Default PostgreSQL port
     }
 }
 
